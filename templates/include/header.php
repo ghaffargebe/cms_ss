ea44e4b8c5fa39ff8ed69e3f21f69e011bd14fb3
ghaffargebe <ghaffargebe@gmail.com>
2015-04-01 08:13:08 +0700

    commit 2 versi 1.0

--- templates/include/header.php
+++ templates/include/header.php
@@ -30,9 +30,9 @@
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
           <!-- Sidebar toggle button-->
-          <!-- <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
-          </a> -->
+          </a>
           <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
               <!-- Messages: style can be found in dropdown.less-->
